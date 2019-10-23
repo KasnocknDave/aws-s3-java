795a74679eee654f4cda571cc228a3415934cfa8
KasnocknDave <dhn@xxxlgroup.com>
2019-10-23 15:55:34 +0200

    Add support for testNG version 7.0.0 as test suit

--- src/test/java/org/aws/s3/bucketfetcher/AppTest.java
+++ src/test/java/org/aws/s3/bucketfetcher/AppTest.java
@@ -1,38 +1,16 @@
 package org.aws.s3.bucketfetcher;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+/** Unit test for simple App. */
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @BeforeClass(description = "code that will be invoked when this test is instantiated")
+    public void setUp() {}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test(description = "Rigourous Test :-)")
+    public void testApp() {
+        assert true;
     }
 }
